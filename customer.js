08a19fe464d8f7a5750733d9308f1907926b2841
nquyenancu <63906680+nquyenancu@users.noreply.github.com>
2023-03-04 21:37:03 +0700

    fix

--- customer.js
+++ customer.js
@@ -1,4 +1,4 @@
-window.onload = function() 
+$(document).ready(function(){
 	console.log("OKE")
 	document.getElementsByClassName('hope-c-PJLV-ihaOegS-css')[0].style.gridTemplateColumns='repeat(auto-fill, minmax(150px, 1fr))';
 	const boxes = document.querySelectorAll('.hope-c-PJLV-iiRGVPv-css');
@@ -7,4 +7,8 @@ window.onload = function()
 	  box.style.height = 'auto';
 	  box.style.width = '100%';
 	});
-};
+});
+
+function bindEvents(){
+	console.log("bindEvents")
+}
