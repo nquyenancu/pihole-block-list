dbe690c23081dcf74c80cb23d599a94111ccab51
nquyenancu <63906680+nquyenancu@users.noreply.github.com>
2023-03-04 20:35:23 +0700

    fix

--- customer.js
+++ customer.js
@@ -1,7 +1,10 @@
-document.getElementsByClassName('hope-c-PJLV-ihaOegS-css')[0].style.gridTemplateColumns='repeat(auto-fill, minmax(150px, 1fr))';
-const boxes = document.querySelectorAll('.hope-c-PJLV-iiRGVPv-css');
+window.addEventListener('load', thing);
+function thing() {
+	document.getElementsByClassName('hope-c-PJLV-ihaOegS-css')[0].style.gridTemplateColumns='repeat(auto-fill, minmax(150px, 1fr))';
+	const boxes = document.querySelectorAll('.hope-c-PJLV-iiRGVPv-css');
 
-boxes.forEach(box => {
-  box.style.height = 'auto';
-  box.style.width = '100%';
-});
+	boxes.forEach(box => {
+	  box.style.height = 'auto';
+	  box.style.width = '100%';
+	});
+}
