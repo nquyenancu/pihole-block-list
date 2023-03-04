1de8910bc73775396a83dcc0098ea397726de378
nquyenancu <63906680+nquyenancu@users.noreply.github.com>
2023-03-04 20:27:39 +0700

    add file

---
+++ customer.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName('hope-c-PJLV-ihaOegS-css')[0].style.gridTemplateColumns='repeat(auto-fill, minmax(150px, 1fr))';
+const boxes = document.querySelectorAll('.hope-c-PJLV-iiRGVPv-css');
+
+boxes.forEach(box => {
+  box.style.height = 'auto';
+  box.style.width = '100%';
+});
